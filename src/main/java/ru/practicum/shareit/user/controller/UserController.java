7d3cc898f148d5fd94c88f3be575fd82a996768e
Eleonora <106757028+Eleonora-Y@users.noreply.github.com>
2023-04-21 02:21:58 +0300

    Update UserController.java

--- src/main/java/ru/practicum/shareit/user/controller/UserController.java
+++ src/main/java/ru/practicum/shareit/user/controller/UserController.java
@@ -19,32 +19,31 @@ public class UserController {
 
     @PostMapping
     public UserDto createUser(@Valid @RequestBody UserDto userDto) {
-        log.info("Создание нового пользователя.");
+        log.debug("POST-запрос на создание нового пользователя.");
         return userService.create(userDto);
     }
 
     @GetMapping("/{userId}")
     public UserDto getUserById(@PathVariable Long userId) {
-        log.info("Вывод пользователя по идентификатору.");
+        log.debug("GET-запрос на вывод пользователя по идентификатору.");
         return userService.getUserById(userId);
     }
 
     @GetMapping
     public List<UserDto> getAllUsers() {
-        log.info("Вывод всех пользователей.");
+        log.debug("GET-запрос на вывод всех пользователей.");
         return userService.getAll();
     }
 
     @PatchMapping("/{userId}")
     public UserDto updateUser(@PathVariable Long userId, @RequestBody UserDto userDto) {
-        log.info("Обновление пользователя.");
+        log.debug("PATCH-запрос на обновление пользователя.");
         return userService.update(userId, userDto);
     }
 
     @DeleteMapping("/{userId}")
     public void deleteUserById(@PathVariable Long userId) {
-        log.info("Удаление пользователя.");
+        log.debug("DELETE-запрос на удаление пользователя.");
         userService.deleteUserById(userId);
     }
 }
-
