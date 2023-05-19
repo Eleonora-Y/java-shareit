ae05af5abd876e2355946e0a07b0a68167c19130
Eleonora <106757028+Eleonora-Y@users.noreply.github.com>
2023-05-19 17:30:00 +0300

    feat: создавать запрос вещи и добавлять в ответ

--- src/main/java/ru/practicum/shareit/request/controller/ItemRequestController.java
+++ src/main/java/ru/practicum/shareit/request/controller/ItemRequestController.java
@@ -1,12 +1,45 @@
 package ru.practicum.shareit.request.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.request.dto.ItemRequestDto;
+import ru.practicum.shareit.request.service.ItemRequestService;
 
-/**
- * TODO Sprint add-item-requests.
- */
+import javax.validation.Valid;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.PositiveOrZero;
+import java.util.List;
+
+
 @RestController
 @RequestMapping(path = "/requests")
+@RequiredArgsConstructor
+@Validated
 public class ItemRequestController {
+    private final ItemRequestService requestService;
+
+    @PostMapping
+    public ItemRequestDto create(@RequestHeader("X-Sharer-User-Id") Long userId,
+                                 @Valid @RequestBody ItemRequestDto itemRequestDto) {
+        return requestService.create(itemRequestDto, userId);
+    }
+
+    @GetMapping("{requestId}")
+    public ItemRequestDto findRequestById(@RequestHeader("X-Sharer-User-Id") Long userId,
+                                          @PathVariable Long requestId) {
+        return requestService.findById(userId, requestId);
+    }
+
+    @GetMapping("/all")
+    public List<ItemRequestDto> findAllRequests(@RequestHeader("X-Sharer-User-Id") Long userId,
+                                                @PositiveOrZero @RequestParam(defaultValue = "0") Integer from,
+                                                @Positive @RequestParam(defaultValue = "10") Integer size) {
+        return requestService.findAllRequests(userId, from, size);
+    }
+
+    @GetMapping
+    public List<ItemRequestDto> findAllUserRequests(@RequestHeader("X-Sharer-User-Id") Long userId) {
+        return requestService.findAllUserRequests(userId);
+    }
 }
