9ada17065536e6af966202b0e1fa14c66ccfdcae
Eleonora <106757028+Eleonora-Y@users.noreply.github.com>
2023-05-29 19:07:34 +0300

    feat: создавать запрос вещи и добавлять в ответ

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -3,6 +3,7 @@ package ru.practicum.shareit.item.model;
 import lombok.*;
 import ru.practicum.shareit.booking.dto.ShortItemBookingDto;
 import ru.practicum.shareit.item.comment.dto.CommentDto;
+import ru.practicum.shareit.request.model.ItemRequest;
 
 import javax.persistence.*;
 import java.util.List;
@@ -31,5 +32,8 @@ public class Item {
     private ShortItemBookingDto nextBooking;
     @Transient
     private List<CommentDto> comments;
+    @ManyToOne
+    @JoinColumn(name = "request_id")
+    private ItemRequest itemRequest;
 
 }
---
+++ src/main/java/ru/practicum/shareit/request/service/ItemRequestServiceImpl.java
@@ -0,0 +1,72 @@
+package ru.practicum.shareit.request.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.practicum.shareit.exception.NotFoundException;
+import ru.practicum.shareit.item.repository.ItemRepository;
+import ru.practicum.shareit.request.dto.ItemRequestDto;
+import ru.practicum.shareit.request.mapper.ItemRequestMapper;
+import ru.practicum.shareit.request.model.ItemRequest;
+import ru.practicum.shareit.request.repository.ItemRequestRepository;
+import ru.practicum.shareit.user.mapper.UserMapper;
+import ru.practicum.shareit.user.service.UserServiceImpl;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class ItemRequestServiceImpl implements ItemRequestService {
+    private final ItemRequestRepository requestRepository;
+    private final UserServiceImpl userService;
+    private final ItemRepository itemRepository;
+
+    @Override
+    @Transactional
+    public ItemRequestDto create(ItemRequestDto itemRequestDto, Long userId) {
+        ItemRequest itemRequest = ItemRequest.builder()
+                .description(itemRequestDto.getDescription())
+                .requester(UserMapper.toUser(userService.findUserById(userId)))
+                .created(LocalDateTime.now())
+                .build();
+        return ItemRequestMapper.toItemRequestDto(requestRepository.save(itemRequest));
+    }
+
+    @Override
+    @Transactional
+    public ItemRequestDto findById(Long userId, Long requestId) {
+        ItemRequest itemRequest = requestRepository.findById(requestId)
+                .orElseThrow(() -> new NotFoundException(String.format("Request with id = %d not found.", requestId)));
+        itemRequest.setItems(itemRepository.findAllByItemRequest(itemRequest));
+        ItemRequestDto itemRequestDto = ItemRequestMapper.toItemRequestDto(itemRequest);
+        itemRequestDto.setRequester(userService.findUserById(userId));
+        return itemRequestDto;
+    }
+
+    @Override
+    @Transactional
+    public List<ItemRequestDto> findAllRequests(Long userId, int from, int size) {
+        UserMapper.toUser(userService.findUserById(userId));
+        Pageable page = PageRequest.of(from / size, size, Sort.by("created"));
+        return requestRepository.findAllByRequesterIdIsNot(userId, page).stream()
+                .peek(itemRequest -> itemRequest.setItems(itemRepository.findAllByItemRequest(itemRequest)))
+                .map(ItemRequestMapper::toItemRequestDto)
+                .collect(Collectors.toList());
+
+    }
+
+    @Override
+    @Transactional
+    public List<ItemRequestDto> findAllUserRequests(Long userId) {
+        userService.findUserById(userId);
+        return requestRepository.findAllByRequesterIdOrderByCreatedDesc(userId).stream()
+                .peek(i -> i.setItems(itemRepository.findAllByItemRequest(i)))
+                .map(ItemRequestMapper::toItemRequestDto)
+                .collect(Collectors.toList());
+    }
+}
