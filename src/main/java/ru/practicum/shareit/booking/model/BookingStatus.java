8f8fc783e57e866806559389c2e3068fcfca9c61
Eleonora <106757028+Eleonora-Y@users.noreply.github.com>
2023-06-11 19:02:00 +0300

    Delete src directory

--- src/main/java/ru/practicum/shareit/booking/model/BookingStatus.java
+++
@@ -1,5 +0,0 @@
-package ru.practicum.shareit.booking.model;
-
-public enum BookingStatus {
-    WAITING, APPROVED, REJECTED, COMPLETED
-}
--- src/main/java/ru/practicum/shareit/exception/TimeDataException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.exception;
-
-public class TimeDataException extends BadRequestException {
-    public TimeDataException(String message) {
-        super(message);
-    }
-}
