d323aab77e0385d99dbb94fcfb7a8f193ae0ccbd
Eleonora <106757028+Eleonora-Y@users.noreply.github.com>
2023-06-12 23:21:17 +0300

    Add files via upload

--- server/src/main/java/ru/practicum/shareit/request/repository/ItemRequestRepository.java
+++ server/src/main/java/ru/practicum/shareit/request/repository/ItemRequestRepository.java
@@ -2,12 +2,10 @@ package ru.practicum.shareit.request.repository;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import ru.practicum.shareit.request.model.ItemRequest;
 
 import java.util.List;
 
-@Repository
 public interface ItemRequestRepository extends JpaRepository<ItemRequest, Long> {
     List<ItemRequest> findAllByRequesterIdOrderByCreatedDesc(long userId);
 
--- server/src/main/java/ru/practicum/shareit/request/service/ItemRequestServiceImpl.java
+++ server/src/main/java/ru/practicum/shareit/request/service/ItemRequestServiceImpl.java
@@ -38,7 +38,7 @@ public class ItemRequestServiceImpl implements ItemRequestService {
     }
 
     @Override
-    @Transactional(readOnly = true)
+    @Transactional
     public ItemRequestDto findById(Long userId, Long requestId) {
         ItemRequest itemRequest = requestRepository.findById(requestId)
                 .orElseThrow(() -> new NotFoundException(String.format("Request with id = %d not found.", requestId)));
@@ -49,7 +49,7 @@ public class ItemRequestServiceImpl implements ItemRequestService {
     }
 
     @Override
-    @Transactional(readOnly = true)
+    @Transactional
     public List<ItemRequestDto> findAllRequests(Long userId, int from, int size) {
         UserMapper.toUser(userService.findUserById(userId));
         Pageable page = PageRequest.of(from / size, size, Sort.by("created"));
@@ -61,7 +61,7 @@ public class ItemRequestServiceImpl implements ItemRequestService {
     }
 
     @Override
-    @Transactional(readOnly = true)
+    @Transactional
     public List<ItemRequestDto> findAllUserRequests(Long userId) {
         userService.findUserById(userId);
         return requestRepository.findAllByRequesterIdOrderByCreatedDesc(userId).stream()
