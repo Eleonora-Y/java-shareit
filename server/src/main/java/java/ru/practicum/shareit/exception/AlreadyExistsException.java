f32fa34465d8585e2e16424c92646cb53c5c3fcd
Eleonora <106757028+Eleonora-Y@users.noreply.github.com>
2023-06-12 21:06:44 +0300

    Delete server/src/main/java/java/ru/practicum/shareit directory

--- server/src/main/java/java/ru/practicum/shareit/exception/AlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.exception;
-
-public class AlreadyExistsException extends BadRequestException {
-    public AlreadyExistsException(String message) {
-        super(message);
-    }
-}
--- server/src/main/java/java/ru/practicum/shareit/exception/OperationAccessException.java
+++
@@ -1,8 +0,0 @@
-package ru.practicum.shareit.exception;
-
-public class OperationAccessException extends RuntimeException {
-    public OperationAccessException(String message) {
-        super(message);
-    }
-}
-
